fed1ef146f8c7c99e77eff09ec047b6b13e2a1f8
shinohara <>
2022-05-25 14:38:58 +0900

    Task.5 LINEで送ったメッセージをタスクとして登録するAPIを作成する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'line_bot/callback'
+  post 'callback', to: 'line_bot#callback'
   resources :tasks
   get '', to: 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
